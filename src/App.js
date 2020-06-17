b50808a956d77fabc3308021515bf0057b927f3c
ellen sierens <ellen.sierens@student.howest.be>
2020-06-17 15:15:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Fullscreen from "react-full-screen";
+import socketIOClient from "socket.io-client";
+import Example from "./Example";
+import "./reset.css";
+import "./style.css";
+
+const ENDPOINT = "https://evening-caverns-60077.herokuapp.com/";
+// const ENDPOINT = "http://127.0.0.1:8081";
 
 function App() {
+
+  // let fullScreenRef = useRef(null);
+  let [fullScreenMode, setfullScreenMode]= useState(false);
+
+  const socket = socketIOClient(ENDPOINT);
+  // const [response, setResponse] = useState("");
+
+  // useEffect(() => {
+  //   // const socket = socketIOClient(ENDPOINT);
+  //   // console.log(document.body.div)
+  //   // document.body.requestFullscreen();
+  //   socket.on("coords", (data) => {
+  //     console.log(data);
+  //     console.log(data.longitude);
+  //     console.log(data.latitude);
+  //   });
+
+  //   // setfullScreenMode(!fullScreenMode);
+  // }, []);
+
+  const fullScreenToggler = () => {
+    setfullScreenMode(!fullScreenMode);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fullscreen enabled={fullScreenMode}>
+    {/* <div onLoad={fullScreenToggler}> */}
+      <button className="fullscreen__button" onClick={fullScreenToggler}>
+             FullScreen Mode
+          </button>
+      <div className="wifi__container">
+        <img alt="wifi icon" className="wifi" src="./assets/wifi.svg" ></img>
+      </div>
+      {/* <button onClick={(e) => goFullScreen()}> enable full screen </button> */}
+      {/* <p> */}
+      {/* It's <time dateTime={response}>{response}</time> */}
+      {/* </p> */}
+      {/* <input type="range" min="78" max="102" onInput={handleChangeSlider}></input> */}
+      <Example
+        className={"nipple1"}
+        color={"#00204B"}
+        border={"none"}
+        socket={socket}
+        name={"cameraControls"}
+      />
+      <Example
+        className={"nipple2"}
+        color={"#FFB400"}
+        border={"none"}
+        socket={socket}
+        name={"carControls"}
+      />
+    {/* </div> */}
+    </Fullscreen>
+    // TODO: input eventlistener 'input' needs to trigger emit to server
   );
 }
 
---
+++ src/Example.js
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactNipple from "react-nipple";
+import "./style.css";
+
+// optional: include the stylesheet somewhere in your app
+import "react-nipple/lib/styles.css";
+
+class Example extends React.Component {
+  constructor({ color, className, border, socket, name }) {
+    super();
+    this.color = color;
+    this.border = border;
+    this.className = className;
+    this.socket = socket;
+    this.name = name;
+  }
+
+  // handleChangeSlider(e) {
+  //   console.log('in change');
+  //   console.log(e.currentTarget.value);
+  //   this.socket.emit("changeServo", (e.currentTarget.value));
+  //   // if(e.currentTarget.value > 90) {
+  //   //   socket.emit("right", (e.currentTarget.value));
+  //   // }else {
+  //   //   socket.emit("left");
+  //   // }
+  // }
+  
+  render() {
+    return (
+      <>
+        <div className={this.className}>
+          <ReactNipple
+            // supports all nipplejs options
+            // see https://github.com/yoannmoinet/nipplejs#options
+            options={{ mode: "dynamic", color: this.color }}
+            // any unknown props will be passed to the container element, e.g. 'title', 'style' etc
+            style={{
+              outline: this.border,
+              width: "50vw",
+              height: "100vh",
+              // if you pass position: 'relative', you don't need to import the stylesheet
+            }}
+            // all events supported by nipplejs are available as callbacks
+            // see https://github.com/yoannmoinet/nipplejs#start
+            // onStart={
+            //   () => {
+            //     this.socket.emit(this.name, "stop");
+            // }}
+            onEnd={
+              () => {
+                this.socket.emit("stop", 0);
+            }}
+            onMove={(evt, data) => {
+              console.log(data)
+              console.log(data.instance.off)
+              this.socket.emit(this.name, (data.instance.frontPosition));
+            }}
+          />
+        </div>
+      </>
+    );
+  }
+}
+export default Example;
