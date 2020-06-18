7011a09136e215019f8e71935b26b94842825260
ellen sierens <ellen.sierens@student.howest.be>
2020-06-18 16:47:07 +0200

    retry

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ const ENDPOINT = "https://evening-caverns-60077.herokuapp.com/";
 // const ENDPOINT = "http://127.0.0.1:8081";
 
 function App() {
-
   // let fullScreenRef = useRef(null);
-  let [fullScreenMode, setfullScreenMode]= useState(false);
+  let [fullScreenMode, setfullScreenMode] = useState(false);
 
   const socket = socketIOClient(ENDPOINT);
   // const [response, setResponse] = useState("");
@@ -25,47 +24,45 @@ function App() {
     //   console.log(data.longitude);
     //   console.log(data.latitude);
     // });
-    socket.emit("controllerConnected");
 
     // setfullScreenMode(!fullScreenMode);
   }, []);
 
   const fullScreenToggler = () => {
     setfullScreenMode(!fullScreenMode);
-  }
-
+  };
 
   return (
     <>
-    <Fullscreen enabled={fullScreenMode}>
-    {/* <div onLoad={fullScreenToggler}> */}
-      <button className="fullscreen__button" onClick={fullScreenToggler}>
-             FullScreen Mode
-          </button>
-      <div className="wifi__container">
-        <img alt="wifi icon" className="wifi" src="./assets/wifi.svg" ></img>
-      </div>
-      {/* <button onClick={(e) => goFullScreen()}> enable full screen </button> */}
-      {/* <p> */}
-      {/* It's <time dateTime={response}>{response}</time> */}
-      {/* </p> */}
-      {/* <input type="range" min="78" max="102" onInput={handleChangeSlider}></input> */}
-      <Example
-        className={"nipple1"}
-        color={"#00204B"}
-        border={"none"}
-        socket={socket}
-        name={"cameraControls"}
-      />
-      <Example
-        className={"nipple2"}
-        color={"#FFB400"}
-        border={"none"}
-        socket={socket}
-        name={"carControls"}
-      />
-    {/* </div> */}
-    </Fullscreen>
+      <Fullscreen enabled={fullScreenMode}>
+        {/* <div onLoad={fullScreenToggler}> */}
+        <button className="fullscreen__button" onClick={fullScreenToggler}>
+          FullScreen Mode
+        </button>
+        <div className="wifi__container">
+          <img alt="wifi icon" className="wifi" src="./assets/wifi.svg"></img>
+        </div>
+        {/* <button onClick={(e) => goFullScreen()}> enable full screen </button> */}
+        {/* <p> */}
+        {/* It's <time dateTime={response}>{response}</time> */}
+        {/* </p> */}
+        {/* <input type="range" min="78" max="102" onInput={handleChangeSlider}></input> */}
+        <Example
+          className={"nipple1"}
+          color={"#00204B"}
+          border={"none"}
+          socket={socket}
+          name={"cameraControls"}
+        />
+        <Example
+          className={"nipple2"}
+          color={"#FFB400"}
+          border={"none"}
+          socket={socket}
+          name={"carControls"}
+        />
+        {/* </div> */}
+      </Fullscreen>
     </>
     // TODO: input eventlistener 'input' needs to trigger emit to server
   );
